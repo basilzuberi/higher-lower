76f339b52d10b5e19811cb2e37d35bd6892b5f1f
Basil Zuberi <basilzuberi@hotmail.com>
2021-12-17 02:52:56 -0500

    Query Likes and give points based upon result

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:youtube_api/youtube_api.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:math';
+import 'package:http/http.dart' as http;
 
-String query = "PewDiePie";
+String query = "Fireship";
 late List<YouTubeVideo> videoResult;
 String youTubeKey = dotenv.env['YOUTUBE_API_KEY'].toString();
 int max = 50;
 YoutubeAPI ytApi = YoutubeAPI(youTubeKey, maxResults: max, type: "channel");
-String topYoutubeVideo = "";
-String botYoutubeVideo = "";
+String strTopYoutubeVideoLink = "";
+String strBotYoutubeVideoLink = "";
+String strTopYoutubeVideoLikes = "";
+String strBotYoutubeVideoLikes = "";
+String strTopVideoID = "";
+String strBotVideoID = "";
 var rng = Random();
 Future main() async {
   // To load the .env file contents into dotenv.
@@ -20,8 +27,35 @@ Future main() async {
   videoResult = await ytApi.search(query);
 
   getVideos(videoResult);
+  strTopYoutubeVideoLikes = await getVideoStats(youTubeKey, strTopVideoID);
+  strBotYoutubeVideoLikes = await getVideoStats(youTubeKey, strBotVideoID);
 
   runApp(const GameApp());
+}
+
+getVideoStats(String apikey, String id) async {
+  String likeCount = "";
+  // This query gets the like counts for a video
+  await http
+      .get(Uri.parse(
+          "https://www.googleapis.com/youtube/v3/videos?part=statistics&id=$id&key=$apikey"))
+      .then((value) {
+    final response = value;
+    if (response.statusCode == 200) {
+      likeCount = json
+          .decode(response.body)['items'][0]['statistics']['likeCount']
+          .toString();
+    } else if (response.statusCode == 403) {
+      likeCount = "-1";
+    }
+  });
+  strTopYoutubeVideoLikes = "-1";
+  return likeCount;
+}
+
+void returnStats() async {
+  strTopYoutubeVideoLikes = await getVideoStats(youTubeKey, strTopVideoID);
+  strBotYoutubeVideoLikes = await getVideoStats(youTubeKey, strBotVideoID);
 }
 
 void getVideos(videoResult) {
@@ -31,8 +65,11 @@ void getVideos(videoResult) {
   String checkQueryVideo2 = videoResult[v2].kind.toString().toLowerCase();
 
   if (checkQueryVideo1 == "video" || checkQueryVideo2 == "video") {
-    topYoutubeVideo = videoResult[v1].thumbnail.medium.url.toString();
-    botYoutubeVideo = videoResult[v2].thumbnail.medium.url.toString();
+    strTopYoutubeVideoLink = videoResult[v1].thumbnail.medium.url.toString();
+    strBotYoutubeVideoLink = videoResult[v2].thumbnail.medium.url.toString();
+    strTopVideoID = videoResult[v1].id;
+    strBotVideoID = videoResult[v2].id;
+    returnStats();
   }
 
   while (checkQueryVideo1 != "video" || checkQueryVideo2 != "video") {
@@ -41,8 +78,12 @@ void getVideos(videoResult) {
     checkQueryVideo1 = videoResult[v1].kind.toString().toLowerCase();
     checkQueryVideo2 = videoResult[v2].kind.toString().toLowerCase();
 
-    topYoutubeVideo = videoResult[v1].thumbnail.medium.url.toString();
-    botYoutubeVideo = videoResult[v2].thumbnail.medium.url.toString();
+    strTopYoutubeVideoLink = videoResult[v1].thumbnail.medium.url.toString();
+    strBotYoutubeVideoLink = videoResult[v2].thumbnail.medium.url.toString();
+
+    strTopVideoID = videoResult[v1].id;
+    strBotVideoID = videoResult[v2].id;
+    returnStats();
   }
 }
 
@@ -71,18 +112,38 @@ class HomePageWidget extends StatefulWidget {
 
 class _HomePageWidgetState extends State<HomePageWidget> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
-
   int _score = 0;
 
-  void _incrementSCore() {
+  void _incrementScore(String state) {
+    int topLikes = int.parse(strTopYoutubeVideoLikes);
+    int botLikes = int.parse(strBotYoutubeVideoLikes);
     setState(() {
-      _score++;
+      if ((state == "up" && topLikes >= botLikes) ||
+          (state == "down" && botLikes >= topLikes)) {
+        _score++;
+      } else {
+        _score = 0;
+      }
     });
+
     getVideos(videoResult);
+  }
+
+  _showErrorToast(BuildContext context) {
+    final scaffold = ScaffoldMessenger.of(context);
+    scaffold.showSnackBar(
+      SnackBar(
+        content: const Text(
+            'API Error, Try Again Later... MAXIMUM Queries Attempted'),
+        action: SnackBarAction(
+            label: 'OK', onPressed: scaffold.hideCurrentSnackBar),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    // strTopYoutubeVideoLikes="-1";
     return Scaffold(
       key: scaffoldKey,
       body: SingleChildScrollView(
@@ -92,6 +153,12 @@ class _HomePageWidgetState extends State<HomePageWidget> {
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Text(
+                  'Videos for: $query',
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0),
+                ),
                 Padding(
                   padding: const EdgeInsets.all(12.0),
                   child: Container(
@@ -102,14 +169,16 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                       ),
                     ),
                     child: Image.network(
-                      topYoutubeVideo,
+                      strTopYoutubeVideoLink,
                       scale: 1,
                     ),
                   ),
                 ),
-                const Text("12386 Likes"),
+                (strTopYoutubeVideoLikes == "-1")
+                    ? _showErrorToast(context)
+                    : Text('$strTopYoutubeVideoLikes Likes'),
                 const Text(
-                  'Is Higher or Lower than:',
+                  'Which Has More Likes:',
                   textAlign: TextAlign.center,
                   overflow: TextOverflow.ellipsis,
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0),
@@ -132,7 +201,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                     splashColor: Colors.black54,
                     onPressed: () {
                       print("PRESSED UP");
-                      _incrementSCore();
+                      _incrementScore("up");
                     },
                   ),
                 ),
@@ -158,7 +227,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                     splashColor: Colors.black54,
                     onPressed: () {
                       print("PRESSED DOWN");
-                      _incrementSCore();
+                      _incrementScore("down");
                     },
                   ),
                 ),
@@ -172,12 +241,12 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                       ),
                     ),
                     child: Image.network(
-                      botYoutubeVideo,
+                      strBotYoutubeVideoLink,
                       scale: 1,
                     ),
                   ),
                 ),
-                const Text("1234 Likes"),
+                const Text('???? Likes'),
               ],
             ),
           ),
