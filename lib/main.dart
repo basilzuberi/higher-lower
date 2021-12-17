e7ca83a5c7c9a038ebc8ebad79e8bb76d92c4eb9
Basil Zuberi <basilzuberi@hotmail.com>
2021-12-16 21:42:33 -0500

    Change UI elements

--- lib/main.dart
+++ lib/main.dart
@@ -27,90 +27,104 @@ class HomePageWidget extends StatefulWidget {
 
 class _HomePageWidgetState extends State<HomePageWidget> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
+  int _score = 0;
+  void _incrementSCore() {
+    setState(() {
+      _score++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
-      backgroundColor: Colors.blue,
-      body: SafeArea(
-        child: Align(
-          alignment: AlignmentDirectional(0, 0),
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 1,
-            decoration: BoxDecoration(
-              color: Colors.lightBlue,
-              shape: BoxShape.rectangle,
-            ),
-            child: Stack(
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Align(
-                      alignment: AlignmentDirectional(0, 0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding:
-                                EdgeInsetsDirectional.fromSTEB(20, 20, 20, 20),
-                            child: Image.network(
-                              'https://picsum.photos/seed/44/600',
-                              width: 100,
-                              height: 100,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          Padding(
-                            padding:
-                                EdgeInsetsDirectional.fromSTEB(20, 20, 20, 20),
-                            child: Image.network(
-                              'https://picsum.photos/seed/696/600',
-                              width: 100,
-                              height: 100,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          print('Button pressed ...');
-                        },
-                        child: const Text("Higher"),
-                        style: ElevatedButton.styleFrom(
-                          fixedSize: Size(200, 40),
-                          elevation: 0.5,
-                          primary: Colors.blueGrey,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          print('Button pressed ...');
-                        },
-                        child: const Text("Lower"),
-                        style: ElevatedButton.styleFrom(
-                          fixedSize: Size(200, 40),
-                          elevation: 0.5,
-                          primary: Colors.blueGrey,
-                        ),
-                      ),
+                    Image.network(
+                      'https://picsum.photos/seed/44/600',
+                      scale: 3,
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+              const Text("12386 Likes"),
+              const Text(
+                'Is Higher or Lower than:',
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FloatingActionButton(
+                  elevation: 1.5,
+                  backgroundColor: Colors.white10,
+                  shape: const StadiumBorder(
+                    side: BorderSide(
+                      color: Colors.black,
+                      width: 1,
+                    ),
+                  ),
+                  child: Image.asset(
+                    "appIcons/arrow.png",
+                    color: Colors.black,
+                  ),
+                  splashColor: Colors.black54,
+                  onPressed: () {
+                    print("PRESSED UP");
+                    _incrementSCore();
+                  },
+                ),
+              ),
+              Text('Score: $_score'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FloatingActionButton(
+                  elevation: 1.5,
+                  backgroundColor: Colors.white10,
+                  shape: const StadiumBorder(
+                    side: BorderSide(
+                      color: Colors.black,
+                      width: 1,
+                    ),
+                  ),
+                  child: RotatedBox(
+                    quarterTurns: 2,
+                    child: Image.asset(
+                      "appIcons/arrow.png",
+                      color: Colors.black,
+                    ),
+                  ),
+                  splashColor: Colors.black54,
+                  onPressed: () {
+                    print("PRESSED DOWN");
+                    _incrementSCore();
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.network(
+                      'https://picsum.photos/seed/44/600',
+                      scale: 3,
+                    ),
+                  ],
+                ),
+              ),
+              const Text("1234 Likes"),
+            ],
           ),
         ),
       ),
